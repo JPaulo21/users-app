d453c401c14149828b4a7f97367937c3c4d33b4e
João Paulo <jp.sport17@gmail.com>
2024-07-21 19:53:49 -0300

    refactor: Delegando responsabilidades

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,36 @@
-const cadastrar = document.querySelector('.btn__cadastrar');
-const api_usuarios = 'http://localhost:8080/users'
+import { notifications } from "./utils/notifications.js";
+import { usuarioService } from "./service/usuario-service.js";
 
-cadastrar.addEventListener('click', (e) => {
-    e.preventDefault();
-    const form = document.querySelector('form');
-    const formData = new FormData(form);
-    const data = Object.fromEntries(formData);
-
-    fetch(api_usuarios,{
-        method:'POST',
-
-        headers: {
-            'Accept':'application/json',
-            'Content-Type':'application/json'
-        },
-
-        body: JSON.stringify(data)
-    }).then((response) => {
-        //console.log(response.headers.get('location')); Obtendo Location do ResponseHeadears
-        if(response.status == 400){
-            const inputUsername = document.getElementById('idUsername');
-            inputUsername.classList.add('error');
-            return response.json();
+function valueItemNavIsSelected(){
+    const inputNavs = document.getElementsByClassName('nav');
+    for(let itemNav of inputNavs){
+        const tab = itemNav.querySelector('#tabCadastrar');
+        if(tab.checked){
+            return tab.value;
         }
-   
-    }).then((jsonData) => {
-        const [username, password] = jsonData;
+    }
+}
 
-        const msgError = document.getElementById('msgErro');
-        msgError.classList.add('msgError');
-        msgError.innerHTML = username.msg;
-    }).catch((error) => {
-        
-    })    
-        
+const btnEnviar = document.querySelector('.btn__enviar');
+
+btnEnviar.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const form = document.querySelector('[data-form-user]');
+    const formDataUser = new FormData(form);
+    const dataUser = Object.fromEntries(formDataUser);
+
+    const itemNav = valueItemNavIsSelected();
+
+    if(itemNav === 'cadastrar'){
+        await usuarioService.salvar(dataUser).then((isSalvo)=>{
+            if(isSalvo)
+                notifications.msgSucess('Usuário criado com sucesso');
+        });
+    }
+
+    if(itemNav === 'auth'){
+
+    }
+
 });
 
